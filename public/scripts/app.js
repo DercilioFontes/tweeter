9a5ac81f82cb7ed74ec586af041f3e13fac0d47e
DercilioFontes <dercilioafontes@gmail.com>
2018-02-02 23:10:56 +0000

    Commit after checked major Functional Requirements, revised code and checked ESLint issues.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,38 +6,6 @@
 
 $(document).ready(function() {
 
-  // Get the data after submitted from the new tweet form 
-  $('.new-tweet form').on('submit', function(event) {
-
-    // Prevent the default behaviour
-    event.preventDefault();
-
-    // Get the data from the form
-    const data = $('.new-tweet form').serialize();
-
-    // Checks if empty or with more then 140 characters and alert the user
-    if (data === 'text=') {
-      alert(`Sorry! You can't submit a empty tweet!`);
-    } else if (data.length > 145) {
-      alert(`Sorry! You can't submit a tweet with more the 140 characters!`);
-    } else {
-
-      // Submit using AJAX
-      $.ajax({ 
-        url: '/tweets', 
-        method: 'POST',
-        data: data, 
-        success: function (result) {
-          // Empties the tweets and loads all tweets again (with the new one sent to database)
-          $('#tweets-container').empty();
-          loadTweets();
-          $('.new-tweet form textarea').val("");
-        }
-      });
-    }
-
-  });
-
   // Gets time in milliseconds and returns how many days ago
   function getDaysAgo(timeX) {
     const currentDate = Date.now();
@@ -47,27 +15,30 @@ $(document).ready(function() {
     return daysAgo.toLocaleString();
   }
 
-  // Creates each tweet and return to be rendered into de site
+  // Creates each tweet and return to be rendered into the site
   function createTweetElement(tweetObj) {
-    const $tweet = $('<article>').addClass("tweet");
+    const $tweet = $("<article>").addClass("tweet");
 
     const $header = $("<header>");
     const $name = $("<h1>").text(tweetObj.user.name);
     const $handle = $("<p>").text(tweetObj.user.handle);
   
-    $header.prepend(`<img alt="avatar" src="${tweetObj.user.avatars.regular}" />`);
+    $header.append(`<img alt="avatar" src="${tweetObj.user.avatars.regular}" />`);
     $header.append($name);
     $header.append($handle);
-    $header.append('<div class="clearfix"></div>');
+    $header.append(`<div class="clearfix"></div>`);
   
     const $main = $("<main>");
-    const $content = $("<p>").text(tweetObj.content.text);
-    $main.append($content);
+    const $mainContent = $("<p>").text(tweetObj.content.text);
+    $main.append($mainContent);
 
     const $footer = $("<footer>");
     const daysAgo = getDaysAgo(tweetObj.created_at);
-    const $date = $('<p>').text(`${daysAgo} days ago`);
+    const $date = $("<p>").text(`${daysAgo} day(s) ago`);
     $footer.append($date);
+    // Add icons
+    $footer.append(`<div class="icons"><i class="fas fa-flag"></i> <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></div>`);
+    $footer.append(`<div class="clearfix"></div>`);
 
 
     $tweet.append($header);
@@ -83,23 +54,55 @@ $(document).ready(function() {
     tweets.forEach(function(tweetObj) {
       const $tweet = createTweetElement(tweetObj);
       // takes return value and prepends it to the tweets container
-      $('#tweets-container').prepend($tweet);
+      $("#tweets-container").prepend($tweet);
     });
   }
 
   // Fetchs tweets from /tweets
   function loadTweets() {
 
-    $.ajax({ 
-      url: '/tweets', 
-      method: 'GET', 
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
       success: function (tweetsJSON) {
         renderTweets(tweetsJSON);
       }
     });
   }
 
+  // Get the data after submitted from the new tweet form
+  $(".new-tweet form").on("submit", function(event) {
+
+    // Prevent the default behaviour
+    event.preventDefault();
+
+    // Get the data from the form
+    const data = $(".new-tweet form").serialize();
+    
+    // Checks if empty or with more then 140 characters and alert the user (145 because of the 'text=' that exist in the data)
+    if (data === "text=") {
+      alert(`Sorry! You can't submit a empty tweet!`);
+    } else if (data.length > 145) {
+      alert(`Sorry! You can't submit a tweet with more the 140 characters!`);
+    } else {
+
+      // Submit using AJAX
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: data,
+        success: function (result) {
+          // Empties the tweets and loads all tweets again (with the new one sent to database)
+          $("#tweets-container").empty();
+          $(".new-tweet form span").text(140);
+          $(".new-tweet form textarea").val("");
+          loadTweets();
+        }
+      });
+    }
+
+  });
+
   loadTweets();
 
 });
-
--- public/scripts/composer-form.js
+++ public/scripts/composer-form.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
-  let totalLength = 140;
 
   // Gets the input in the textarea and counts the characters.
-  $('.new-tweet').children('form').children('textarea').on('keyup', function() {
-    var text  = $(this).val();
+  $(".new-tweet form textarea").on("keyup", function() {
+    const text  = $(this).val();
 
-    // Sets counter of remaining characters and red if negative (and returns black if come back to be positive)
-    var remainingCharacters = totalLength - text.length;
-    if (remainingCharacters < 0) {
-      $(this).parent().children('span').text(remainingCharacters).css("color", "red");
+    // Sets counter of remaining characters and red if negative (and returns black if returns to be positive)
+    if ((140 - text.length) < 0) {
+      $(this).parent().children("span").text(140 - text.length).css("color", "red");
     } else {
-      $(this).parent().children('span').text(remainingCharacters).css("color", "black");
+      $(this).parent().children("span").text(140 - text.length).css("color", "black");
     }
   });
+
 
   // Sets the nav-bar button ("Compose") as toggle to slide showing and hidding the new-tweet form
   $( function() {
@@ -24,4 +23,5 @@ $(document).ready(function() {
   } );
 
   $( ".new-tweet").hide();
-}); 
+  
+});
